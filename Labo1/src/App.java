4ef09ca82bb3c3f97ccabf46e25bb1330d89892f
Shoyur <100440995+Shoyur@users.noreply.github.com>
2022-09-28 19:47:13 -0400

    ...

--- Labo1/src/App.java
+++ Labo1/src/App.java
@@ -1,11 +1,67 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.RandomAccessFile;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.scene.image.Image;
 
 public class App extends Application {
+
+    static int nbFois = 0;
+
+    public static String fichier_txt = "";
+    static BufferedReader tmpReadTexte;   
+    static RandomAccessFile tmpWriteBin;
+    static File fichierBin;
+    // numLivre + titre + 2 + numAuteur + annee + nbPages + categLivre + 2
+    static final int TAILLE_ENREG = 3+45+2+2+4+4+20+2;
+
+    public static void convertirTxtEnBinaire() {
+        String elems[] = new String[6];
+        // numLivre + titre + 2 + numAuteur + annee + nbPages + categLivre + 2
+        // TAILLE_ENREG = 3+45+2+2+4+4+20+2;
+        int numLivre, numAuteur, annee, nbPages;
+        String titre="", categLivre="";
+        try{
+            tmpReadTexte = new BufferedReader(new FileReader(fichier_txt));
+            tmpWriteBin = new RandomAccessFile("src/donnees/livres.bin", "rw");
+            String ligne = tmpReadTexte.readLine();
+            while(ligne != null ){
+                elems = ligne.split(";");
+
+                numLivre = Integer.parseInt(elems[0]);
+                titre = String.format("%1$-45.45s", elems[1]);
+                numAuteur = Integer.parseInt(elems[2]);
+                annee = Integer.parseInt(elems[3]);
+                nbPages = Integer.parseInt(elems[4]);
+                categLivre = String.format("%1$-20.20s", elems[5]);
+
+                tmpWriteBin.writeInt(numLivre);
+                tmpWriteBin.writeUTF(titre);
+                tmpWriteBin.writeInt(numAuteur);
+                tmpWriteBin.writeInt(annee);
+                tmpWriteBin.writeInt(nbPages);
+                tmpWriteBin.writeUTF(categLivre);
+
+                ligne = tmpReadTexte.readLine();
+
+            }
+            tmpReadTexte.close();
+            tmpWriteBin.close();
+        } catch (Exception e) { System.out.println("Erreur à la génération du fichier binaire : " + e.getMessage()); }
+    }
 
     @Override
     public void start(Stage scenePrincipale) {
@@ -22,10 +78,12 @@ public class App extends Application {
     }
 
     public String toutLister() {
-        return "test1 retourné !!!";
+        nbFois += 1;
+        return "Vous avez cliqué " + nbFois + " fois !!!\n" + fichier_txt;
     }
  
     public static void main(String[] args) {
         launch(args);
+        // convertirTxtEnBinaire();
     }
 }
--- Labo1/src/ScenePrincipaleController.java
+++ Labo1/src/ScenePrincipaleController.java
@@ -1,16 +1,52 @@
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
-public class ScenePrincipaleController {
+public class ScenePrincipaleController implements Initializable {
+
+    App app = new App();
 
     @FXML
     private TextFlow liste;
 
     @FXML
     void boutonListerTous(ActionEvent event) {
-        liste.getChildren().addAll(new Text(new App().toutLister()));
+        Text texte = new Text(app.toutLister());
+        texte.setFont(Font.font("Calibri", FontPosture.REGULAR, 13));
+        liste.getChildren().clear();
+        liste.getChildren().addAll(texte);
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new ExtensionFilter("*.txt", "*.txt"));
+        fc.setInitialDirectory(new File(System.getProperty("user.dir")));
+        File f = fc.showOpenDialog(null);
+        if (f != null) {
+            App.fichier_txt = f.getAbsolutePath();
+        }
+        else {
+            String titre = "Erreur";
+            String message = "\nVous deviez absolument sélectionner un fichier texte compatible.\n";
+            message += "Ce programme va s'arrêter.\n";
+            JOptionPane.showMessageDialog(null, message, titre, JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        }
+        
+    }
+
 }
