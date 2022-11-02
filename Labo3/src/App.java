3cd29d44c8e59d9294299909bf3e313c5d886659
Shoyur <100440995+Shoyur@users.noreply.github.com>
2022-11-02 17:49:32 -0400

    ...

--- Labo3/src/App.java
+++ Labo3/src/App.java
@@ -1,3 +1,5 @@
+// import org.scenicview.ScenicView;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,10 +9,12 @@ import javafx.stage.Stage;
 public class App extends Application { 
     @Override
     public void start(Stage stagePrincipal) throws Exception {
-        stagePrincipal.setScene(new Scene(FXMLLoader.load(getClass().getResource("scenePrincipale.fxml"))));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("scenePrincipale.fxml")));
+        stagePrincipal.setScene(scene);
         stagePrincipal.getIcons().add(new Image("medias/frameIcon.png"));
         stagePrincipal.setTitle("[Gestionnaire pour Suzy la disquaire]");
         stagePrincipal.show();
+        // ScenicView.show(scene);
     }
     public static void main(String[] args) { launch(args); }
 }
